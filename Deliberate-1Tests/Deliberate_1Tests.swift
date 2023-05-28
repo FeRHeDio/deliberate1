c34c595d60975fbd59ba4d7ca15a67b6b655d10c
Fernando Putallaz <ferhedio@gmail.com>
2023-05-28 11:46:42 -0300

    viewDidLoad loads news Feed

--- Deliberate-1Tests/Deliberate_1Tests.swift
+++ Deliberate-1Tests/Deliberate_1Tests.swift
@@ -6,11 +6,19 @@
 //
 
 import XCTest
+import UIKit
 
-final class TopHeadlinesViewController {
-     init(loader: Deliberate_1Tests.LoaderSpy) {
-         
-     }
+final class TopHeadlinesViewController: UIViewController {
+    private var loader: Deliberate_1Tests.LoaderSpy?
+    
+    convenience init(loader: Deliberate_1Tests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        loader?.load()
+    }
 }
 
 class Deliberate_1Tests: XCTestCase {
@@ -21,9 +29,22 @@ class Deliberate_1Tests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsNews() {
+        let loader = LoaderSpy()
+        let sut = TopHeadlinesViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
 }
